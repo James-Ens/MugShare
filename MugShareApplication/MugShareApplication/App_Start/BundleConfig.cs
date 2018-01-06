cbdc711689160ba22ffd07da37ed153dfcda9b99
james <james.ens@live.com>
2018-01-06 13:22:08 -0800

    add ignorelist to style bundle

--- MugShareApplication/MugShareApplication/App_Start/BundleConfig.cs
+++ MugShareApplication/MugShareApplication/App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace MugShareApplication
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            bundles.IgnoreList.Clear();
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -45,6 +46,8 @@ namespace MugShareApplication
                       "~/Content/site.css",
                       "~/Content/DataTables/css/jquery.dataTables.min.css",
                       "~/Content/css/DataTables.less"));
+            
+
         }
     }
 }
